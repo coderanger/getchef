2bde545d7fca6b07622cd8cd13064b26068c5fd4
Noah Kantrowitz <noah@coderanger.net>
2013-05-27 18:54:41 -0700

    Initial conversion to be backed by SQL.

--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import itertools
+import os
 
 import pkg_resources
 import requests
 import flask
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.contrib.cache import SimpleCache
+
+from ordered_set import OrderedSet
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['BOOTSTRAP_USE_CDN'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///getchef.db')
 
+db = SQLAlchemy(app)
 cache = SimpleCache()
 
 # Because I want a certain order and because adding new ones will take some actual work
-platforms = [
+all_platforms = [
     ('auto', 'Automatic'),
     ('ubuntu', 'Ubuntu'),
     ('el', 'Red Hat/Fedora'),
@@ -26,8 +32,62 @@ platforms = [
     ('osx', 'Mac OS X'),
     ('windows', 'Windows')
 ]
-platform_aliases = {'solaris': 'solaris2', 'osx': 'mac_os_x'}
+platform_aliases = {'solaris2': 'solaris', 'mac_os_x': 'osx'}
 arch_ranking = ['x86_64', 'i686', 'i386', 'sparc']
+arch_ranking.reverse()
+
+class Package(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    platform = db.Column(db.String(255))
+    platform_version = db.Column(db.String(255))
+    arch = db.Column(db.String(255))
+    chef_version = db.Column(db.String(255))
+    raw_path = db.Column(db.String(255))
+    is_client = db.Column(db.Boolean)
+    is_server = db.Column(db.Boolean)
+    is_uploaded = db.Column(db.Boolean)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+        self.platform = platform_aliases.get(self.platform, self.platform)
+
+    def __repr__(self):
+        return '<Package %r>' % self.raw_path
+
+    def opscode_url(self):
+        return 'https://s3.amazonaws.com/opscode-omnibus-packages'+self.raw_path
+
+    @staticmethod
+    def walk_omnitruck(omnitruck_data):
+        for platform, platform_data in omnitruck_data.iteritems():
+            for platform_version, platform_version_data in platform_data.iteritems():
+                for arch, arch_data in platform_version_data.iteritems():
+                    for chef_version, raw_path in arch_data.iteritems():
+                        yield {
+                            'platform': platform,
+                            'platform_version': platform_version,
+                            'arch': arch,
+                            'chef_version': chef_version,
+                            'raw_path': raw_path,
+                        }
+
+    @classmethod
+    def load_omnitruck(cls, omnitruck_data, is_server=False):
+        for attributes in cls.walk_omnitruck(omnitruck_data):
+            if is_server:
+                attributes['is_server'] = True
+            else:
+                attributes['is_client'] = True
+            row = cls.query.filter_by(**attributes).first()
+            if row is None:
+                db.session.add(cls(**attributes))
+        db.session.commit()
+
+    @classmethod
+    def load_all_omnitruck(cls):
+        cls.load_omnitruck(requests.get('http://www.opscode.com/chef/full_list').json(), False)
+        cls.load_omnitruck(requests.get('http://www.opscode.com/chef/full_server_list').json(), True)
 
 def chef_package_info(*args):
     rv = getattr(flask.g, 'chef_package_info', None)
@@ -39,46 +99,43 @@ def chef_package_info(*args):
         rv = reduce(lambda v, key: v.get(key, {}), args, rv)
     return rv
 
-@app.route('/<platform>/<version>/<arch>/<chef_version>/')
-def render(platform, version, arch, chef_version):
-    versions = []
-    raw_versions = chef_package_info(platform)
-    if raw_versions:
-        for ver, ver_data in sorted(raw_versions.iteritems(), key=lambda v: pkg_resources.parse_version(v[0])):
-            for ar in sorted(ver_data.iterkeys(), key=lambda a: arch_ranking.index(a), reverse=True):
-                versions.append((ver, ar))
-    download_url = 'http://opscode-omnibus-packages.s3.amazonaws.com/%s'%chef_package_info(platform, version, arch, chef_version)
-    return render_template('main.html', version_info=chef_package_info(), 
-                                        platforms=platforms,
-                                        raw_platform=platform,
-                                        platform=platform_aliases.get(platform, platform),
-                                        versions=versions,
-                                        version=version,
-                                        arch=arch,
-                                        chef_version=chef_version,
-                                        download_url=download_url)
-
-@app.route('/<platform>/<version>/<arch>/')
-def render_latest_chef_verison(platform, version, arch):
-    chef_versions = chef_package_info(platform, version, arch)
-    latest = max(itertools.ifilter(lambda v: 'rc' not in v, chef_versions), key=pkg_resources.parse_version) if chef_versions else None
-    return render(platform, version, arch, latest)
-
-@app.route('/<platform>/<version>/')
-def render_default_arch(platform, version):
-    archs = chef_package_info(platform, version)
-    default_arch = min(archs, key=lambda a: arch_ranking.index(a)) if archs else None
-    return render_latest_chef_verison(platform, version, default_arch)
-
-@app.route('/<platform>/')
-def render_latest_version(platform):
-    versions = chef_package_info(platform)
-    latest = max(versions, key=pkg_resources.parse_version) if versions else None
-    return render_default_arch(platform, latest)
-
 @app.route('/')
-def render_default():
-    return render_latest_version('auto')
+@app.route('/<platform>/')
+@app.route('/<platform>/<version>/')
+@app.route('/<platform>/<version>/<arch>/')
+@app.route('/<platform>/<platform_version>/<arch>/<chef_version>/')
+def render(platform='auto', platform_version=None, arch=None, chef_version=None):
+    params = {'platform': platform}
+    if platform_version:
+        params['platform_version'] = platform_version
+    if arch:
+        params['arch'] = arch
+    if chef_version:
+        params['chef_version'] = chef_version
+    packages = list(Package.query.filter_by(**params))
+    platform_version_archs = sorted(
+        db.engine.execute('SELECT DISTINCT platform_version, arch FROM package WHERE platform = :platform', platform=platform),
+        reverse=True,
+        key=lambda (ver, ar): (pkg_resources.parse_version(ver), arch_ranking.index(ar))
+    )
+    platform_versions = OrderedSet(ver for ver, ar in platform_version_archs)
+    archs = OrderedSet(ar for ver, ar in platform_version_archs)
+    chef_versions = sorted(set(pkg.chef_version for pkg in packages), reverse=True, key=pkg_resources.parse_version)
+    if not platform_version:
+        platform_version = iter(platform_versions).next()
+    if not arch:
+        arch = iter(archs).next()
+    if not chef_version:
+        chef_version = chef_versions[0]
+    return render_template('main.html', packages=packages,
+                                        all_platforms=all_platforms,
+                                        platform=platform,
+                                        platform_version=platform_version,
+                                        arch=arch,
+                                        platform_versions=platform_versions,
+                                        archs=archs,
+                                        platform_version_archs=platform_version_archs,
+                                        chef_versions=chef_versions)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ manage.py
@@ -0,0 +1,27 @@
+from flask.ext.script import Manager, Server, Shell, prompt_bool
+
+import main
+from main import app, db, Package
+
+manager = Manager(app)
+manager.add_command('server', Server())
+
+@manager.shell
+def shell():
+    return main.__dict__
+
+@manager.command
+def syncdb():
+    db.create_all()
+
+@manager.command
+def dropdb():
+    if prompt_bool('This will delete all data, are you sure'):
+        db.drop_all()
+
+@manager.command
+def omnitruck_sync():
+    Package.load_all_omnitruck()
+
+if __name__ == '__main__':
+    manager.run()
